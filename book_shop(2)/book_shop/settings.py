67036743695264d8ba6af6b4602a48c489bc4927
Velimudr <velimudr@yandex.ru>
2021-07-12 20:20:38 +0400

    Попытка подключения env-файлов в качестве хранилища настроек, но как работать с этим, пока не понял.

--- book_shop(2)/book_shop/settings.py
+++ book_shop(2)/book_shop/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+from environ import Env
+
+Env.read_env('settings.env')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
